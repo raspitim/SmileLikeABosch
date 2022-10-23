52f840643107a54271a8bbbdbc28326ff9840b0a
PiTim <48213130+raspitim@users.noreply.github.com>
2022-10-23 20:01:34 +0200

    Update settings.py

--- TwiNNinfra/settings.py
+++ TwiNNinfra/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$ng=f703l60=!i6(lcmnkd3qe+1gn*w)x5yrx8+tc+#6grg&eg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["0.0.0.0", "192.168.1.198", "127.0.0.1", "smilelikeabosch.azurewebsites.net", "smilelikeabosch.de"]
 
